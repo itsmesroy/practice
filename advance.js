e345b5b2adb428da090d1254f7eba2ffab9f44b9
itsmesroy <itsmevicky78@gmail.com>
2024-08-02 21:14:04 +0530

    selecting a color

--- advance.js
+++ advance.js
@@ -1,12 +1,17 @@
+// Getting all elements
 const h1Element=document.getElementById("heading");
 const increasingElement1=document.getElementById("increasingElement");
 const decreasingElement1=document.getElementById("decreasingElement");
 const counter1=document.getElementById("counter");
+const counter2Element=document.getElementById("counter2");
+const fnameElement= document.getElementById("fname");
+const submitElement=document.getElementById("submit")
 
-
+// Simple DOM Manipulation
  let see="hi! whatsup";
  h1Element.innerHTML=see;
  
+//  Playing with counter increase and decrease feature through click
 increasingElement1.addEventListener("click",() => {
   let numberIncrease=Number(counter1.innerHTML)+1;
   counter1.innerHTML=numberIncrease;
@@ -15,3 +20,25 @@ decreasingElement1.addEventListener("click" ,() =>{
   let numberDecrease=Number(counter1.innerHTML)-1;
   counter1.innerHTML=numberDecrease;
 })
+
+//  Diplaying whatever is written in the input box
+fnameElement.addEventListener("input",(eventObj) =>{
+    console.log(eventObj);
+    const elementtobeShown=eventObj.target;
+    const number= elementtobeShown.value;
+    if(!number){
+        counter2Element.innerHTML="0";
+    }else{
+        counter2Element.innerHTML=number;
+    }
+    
+    console.log("INPUT EVENT");
+});
+
+// Mock fetch api
+async function getApiData(){
+  let url= await fetch("https://demo0802490.mockable.io/");
+  let data=await url.json;
+  return data;
+}
+let getData =await getApiData();
